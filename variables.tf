0e43335e829e61145348752006b89cd60a9d7c6e
Ilya <ilya5000@yandex.ru>
2022-06-27 10:37:51 +0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gu1mud3en55nhmsp57"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g9sf6g36co2frpt7ck"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd88d14a6790do254kj7"
+}
